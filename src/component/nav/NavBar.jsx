2fa354bbd3a7a84cc38bdda900c7e60368962da4
mauroepce <mauricioeslainerpc@gmail.com>
2023-02-14 20:20:31 -0300

    add link to home in navbar

--- src/component/nav/NavBar.jsx
+++ src/component/nav/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Style from "./NavBar.module.css";
 
 function NavBar() {
   return (
     <div className={Style.navBar}>
+            <Link to="/">
         <div className={Style.title}>
-            <h2>Star Wars Battle App</h2>
+              <h2>Star Wars Battle App</h2>
         </div>
+            </Link>
     </div>
   )
 }
